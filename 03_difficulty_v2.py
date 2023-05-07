1b5a257f41efd8ab1d4a94150a0503e224563426
Jack Daly <dalyj2@middleton.school.nz>
2023-05-07 21:07:36 +1200

    Update 03_difficulty_v2.py

--- 03_difficulty_v2.py
+++ 03_difficulty_v2.py
@@ -1,19 +1,22 @@
-"""Based on 03_difficulty_v1. Added output for invalid input and put it into a loop (e.g. 11, 0.5)
+"""Based on 03_difficulty_v1. Solves for invalid input (e.g. 11, 0.5)
+and puts into loop for testing
 """
+
 
 loop = True
 
 while loop:
     diff = input("From 1-10, 1 being low, how confident are you with Maori numbers: ")
 
-    # Set difficulty to hard if they entered above 5
-    if 6 <= int(diff) <= 10:
-        hard = True
-        loop = False
-    elif 1 <= int(diff) <= 5:
-        hard = False
-        loop = False
-    else:
+    try:
+        diff = int(diff)
+        if 6 <= diff <= 10:
+            hard = True
+            print(f"Hard = {hard}")
+        elif 1 <= diff <= 5:
+            hard = False
+            print(f"Hard = {hard}")
+        else:
+            print("Please enter an integer between 1 and 10")
+    except ValueError:
         print("Please enter an integer between 1 and 10")
-
-print(hard)
