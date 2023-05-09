b9fdbd6997ff8e7f4ed6653a5abc12d09af3380c
Jack Daly <dalyj2@middleton.school.nz>
2023-05-09 20:53:05 +1200

    Create 04_questions_v1.py

---
+++ 04_questions_v1.py
@@ -0,0 +1,30 @@
+"""Write the questions and answers in list form
+"""
+
+# Questions
+questions = {
+    "1": "What is 1 in Maori?",
+    "2": "What is 2 in Maori?",
+    "3": "What is 3 in Maori?",
+    "4": "What is 4 in Maori?",
+    "5": "What is 5 in Maori?",
+    "6": "What is 6 in Maori?",
+    "7": "What is 7 in Maori?",
+    "8": "What is 8 in Maori?",
+    "9": "What is 9 in Maori?",
+    "10": "What is 10 in Maori?"
+}
+
+# Answers to the questions
+answers = {
+    "1": "tahi",
+    "2": "rua",
+    "3": "toru",
+    "4": "wha",
+    "5": "rima",
+    "6": "ono",
+    "7": "whitu",
+    "8": "waru",
+    "9": "iwa",
+    "10": "tekau"
+}
