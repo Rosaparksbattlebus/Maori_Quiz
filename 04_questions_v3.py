07a127f5509ff7050f574462a2e658e122d71725
Jack Daly <dalyj2@middleton.school.nz>
2023-05-17 21:26:27 +1200

    create

--- 04_questions_v3.py
+++ 04_questions_v3.py
@@ -1,10 +1,10 @@
 """Based on 04_questions_v2
 Added score counter
-Added hints
+Added hints for the questions
 """
+
 import random
 
-score = 0
 # Questions
 questions = {
     "1": "What is 1 in Maori?",
@@ -18,6 +18,7 @@ questions = {
     "9": "What is 9 in Maori?",
     "10": "What is 10 in Maori?"
 }
+
 # Answers to the questions
 answers = {
     "1": "tahi",
@@ -36,6 +37,9 @@ answers = {
 question_order = list(questions.keys())
 random.shuffle(question_order)
 
+# Make score variable
+score = 0
+
 # Loop through each question in the shuffled order
 for question_num in question_order:
     # Ask the question
@@ -45,10 +49,25 @@ for question_num in question_order:
     user_answer = input("> ")
 
     # Check the user's answer
-    while user_answer.lower() != answers[question_num]:
-        # If the user's answer is wrong, re-ask the question
-        print("Incorrect. Try again")
-        user_answer = input("> ")
+    if user_answer.lower() == answers[question_num]:
+        # If the user's answer is correct on the first try, add 2 to the score
+        score += 2
+        print("Correct. Nice job!")
+    else:
+        # If the user's answer is incorrect, try again
+        # Give them a hint, telling them the first letter of the answer
+        print("Incorrect. Here's a hint: The first letter of the correct answer is", answers[question_num][0])
 
-    # If the user's answer is correct, move on to the next question
-    print("Correct. Nice Job!")
+        # Give the user another chance to answer the question correctly
+        while user_answer.lower() != answers[question_num]:
+            user_answer = input("> ")
+
+            # If the user's answer is correct on the subsequent try, break the loop
+            if user_answer.lower() == answers[question_num]:
+                print("Correct. Nice job!")
+                break
+
+
+# Display the final score
+print(f"You  finished with a score of {score}")
+
